47824a69445d470210300cede8416634d9382d32
shalant <doug.rosenberg@gmail.com>
2023-09-05 17:00:56 -0500

    in module 39

--- BlazorEcommerce/BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlazorEcommerce.Shared;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorEcommerce.Server.Controllers
@@ -7,18 +8,25 @@ namespace BlazorEcommerce.Server.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly IProductService _productService;
 
-        public ProductController(DataContext context)
+        public ProductController(IProductService productService)
         {
-            _context = context;
+            _productService = productService;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProduct()
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
-            return Ok(products);
+            var result = await _productService.GetProductsAsync();
+            return Ok(result);
+        }
+        
+        [HttpGet("{productId}")]
+        public async Task<ActionResult<ServiceResponse<Product>>> GetProduct(int productId)
+        {
+            var result = await _productService.GetProductAsync(productId);
+            return Ok(result);
         }
 
     }
---
+++ BlazorEcommerce/BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Server.Services.ProductService
+{
+    public interface IProductService
+    {
+        Task<ServiceResponse<List<Product>>> GetProductsAsync();
+        Task<ServiceResponse<Product>> GetProductAsync(int productId);
+    }
+}
