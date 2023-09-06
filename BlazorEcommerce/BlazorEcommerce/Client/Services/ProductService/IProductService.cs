f025ce5d45d4145a7835f19d94edfd0d4bd696ca
shalant <doug.rosenberg@gmail.com>
2023-09-05 22:29:29 -0500

    in the middle of module 47

--- BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -4,8 +4,9 @@ namespace BlazorEcommerce.Client.Services.ProductService
 {
     public interface IProductService
     {
+        event Action ProductsChanged;
         List<Product> Products { get; set; }
-        Task GetProducts();
+        Task GetProducts(string? catogoryURl = null);
         Task<ServiceResponse<Product>> GetProduct(int productId);
     }
 }
--- BlazorEcommerce/BlazorEcommerce/Server/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -6,5 +6,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
     {
         Task<ServiceResponse<List<Product>>> GetProductsAsync();
         Task<ServiceResponse<Product>> GetProductAsync(int productId);
+        Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
+
     }
 }
