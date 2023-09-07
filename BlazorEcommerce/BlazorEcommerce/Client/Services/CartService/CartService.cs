707dac38c6c4bc7a1e78911f5426f54c5adb9b1b
shalant <doug.rosenberg@gmail.com>
2023-09-07 10:55:56 -0500

    working on cart, finished 79

--- BlazorEcommerce/BlazorEcommerce/Client/Services/CartService/CartService.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -5,10 +5,12 @@ namespace BlazorEcommerce.Client.Services.CartService
     public class CartService : ICartService
     {
         private readonly ILocalStorageService _localStorage;
+        private readonly HttpClient _http;
 
-        public CartService(ILocalStorageService localStorage)
+        public CartService(ILocalStorageService localStorage, HttpClient http)
         {
             _localStorage = localStorage;
+            _http = http;
         }
 
         public event Action OnChange;
@@ -34,6 +36,35 @@ namespace BlazorEcommerce.Client.Services.CartService
                 cart = new List<CartItem>();
             }
             return cart;
+        }
+
+        public async Task<List<CartProductResponse>> GetCartProducts()
+        {
+            var cartItems = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            var response = await _http.PostAsJsonAsync("api/cart/products", cartItems);
+            var cartProducts =
+                await response.Content.ReadFromJsonAsync<ServiceResponse<List<CartProductResponse>>>();
+            return cartProducts.Data;
+        }
+
+        public async Task RemoveProductFromCart(int productId, int productTypeId)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            if(cart == null)
+            {
+                return;
+            }
+            
+            var cartItem = cart.Find(x => x.ProductId == productId
+                && x.ProductTypeId == productTypeId);
+            if(cartItem != null)
+            {
+                cart.Remove(cartItem);
+                await _localStorage.SetItemAsync("cart", cart);
+                OnChange.Invoke();
+            }
+
+
         }
     }
 }
