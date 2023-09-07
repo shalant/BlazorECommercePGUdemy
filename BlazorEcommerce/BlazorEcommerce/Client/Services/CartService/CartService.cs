b3541d5bfd10e53335a85289d955981756d510c6
shalant <doug.rosenberg@gmail.com>
2023-09-07 12:47:26 -0500

    finished cart and module 84

--- BlazorEcommerce/BlazorEcommerce/Client/Services/CartService/CartService.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -1,4 +1,5 @@
-﻿using Blazored.LocalStorage;
+﻿using BlazorEcommerce.Shared;
+using Blazored.LocalStorage;
 
 namespace BlazorEcommerce.Client.Services.CartService
 {
@@ -22,7 +23,17 @@ namespace BlazorEcommerce.Client.Services.CartService
             {
                 cart = new List<CartItem>();
             }
-            cart.Add(cartItem);
+
+            var sameItem = cart.Find(x => x.ProductId == cartItem.ProductId &&
+                x.ProductTypeId == cartItem.ProductTypeId);
+            if(sameItem == null)
+            {
+                cart.Add(cartItem);
+            }
+            else
+            {
+                sameItem.Quantity += cartItem.Quantity;
+            }
 
             await _localStorage.SetItemAsync("cart", cart);
             OnChange.Invoke();
@@ -63,8 +74,23 @@ namespace BlazorEcommerce.Client.Services.CartService
                 await _localStorage.SetItemAsync("cart", cart);
                 OnChange.Invoke();
             }
+        }
 
+        public async Task UpdateQuantity(CartProductResponse product)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            if (cart == null)
+            {
+                return;
+            }
 
+            var cartItem = cart.Find(x => x.ProductId == product.ProductId
+                && x.ProductTypeId == product.ProductTypeId);
+            if (cartItem != null)
+            {
+                cartItem.Quantity = product.Quantity;
+                await _localStorage.SetItemAsync("cart", cart);
+            }
         }
     }
 }
