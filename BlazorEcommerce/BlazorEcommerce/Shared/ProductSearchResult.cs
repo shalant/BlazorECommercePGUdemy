363ff9904b607ac49b2d9a29778756d5c6f7458c
shalant <doug.rosenberg@gmail.com>
2023-09-06 22:28:23 -0500

    finished module 73

--- BlazorEcommerce/BlazorEcommerce/Shared/ProductSearchResult.cs
+++ BlazorEcommerce/BlazorEcommerce/Shared/ProductSearchResult.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace BlazorEcommerce.Shared
 {
-    internal class ProductSearchResult
+    public class ProductSearchResult
     {
+        public List<Product> Products { get; set; } = new List<Product>();
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
     }
 }
