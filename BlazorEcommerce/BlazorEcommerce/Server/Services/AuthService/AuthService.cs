a2498ba76badbaa8dd11227cecd7964beb6ca54b
shalant <doug.rosenberg@gmail.com>
2023-09-22 22:28:02 -0500

    Working on Orders, finished #139

--- BlazorEcommerce/BlazorEcommerce/Server/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Services/AuthService/AuthService.cs
@@ -9,11 +9,15 @@ namespace BlazorEcommerce.Server.Services.AuthService
     {
         private readonly DataContext _context;
         private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public AuthService(DataContext context, IConfiguration configuration)
+        public AuthService(DataContext context, 
+            IConfiguration configuration,
+            IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
             _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<ServiceResponse<string>> Login(string email, string password)
@@ -135,5 +139,7 @@ namespace BlazorEcommerce.Server.Services.AuthService
 
             return new ServiceResponse<bool> { Data = true, Message = "Password has been changed" };
         }
+
+        public int GetUserId() => int.Parse(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier));
     }
 }
